1a11e7ed57241222f96c44963fcb7aefeeb0b45e
Martin Rixham <martin.rixham@gmail.com>
2019-04-16 21:36:25 +0100

    Fix loading Datum for web app.

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -4,13 +4,17 @@ requirejs.config({
 
 		jquery: "node_modules/jquery/dist/jquery.min",
 		semantic: "node_modules/semantic-ui-offline/semantic.min",
-		hljs: "node_modules/highlight.js-postbuild/index",
-		Datum: "node_modules/Datum/target/Datum.min"
+		hljs: "node_modules/highlight.js-postbuild/index"
 	},
 	shim: {
 
 		"semantic": { "deps": ["jquery"] }
 	}
+});
+
+define("Datum", [], function() {
+
+	return Datum;
 });
 
 require(["jquery", "js/App", "semantic"], function($, App) {
